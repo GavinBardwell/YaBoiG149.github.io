6336bd94e8ab4e6b0bbecbb08f7d325397eb2db5
Gavin Bardwell <gmbwe110@gmail.com>
2023-07-06 09:22:27 -0400

    added top links

--- JavaScript/ClickCopy.js
+++ JavaScript/ClickCopy.js
@@ -14,10 +14,21 @@ document.getElementById("copyPhone").addEventListener("click", function() {
     alert('Copied phone number: +1(985)502-5130');
 });
 
+document.getElementById("copyPhonePic").addEventListener("click", function() {
+  const textToCopy = "+1(985)502-5130";
+  copyToClipboard(textToCopy);
+  alert('Copied phone number: +1(985)502-5130');
+});
+
 document.getElementById("copyEmail").addEventListener("click", function() {
   const textToCopy = "gmbwe110@gmail.com";
   copyToClipboard(textToCopy);
   alert('Copied email: gmbwe110@gmail.com');
 });
 
+document.getElementById("copyEmailPic").addEventListener("click", function() {
+  const textToCopy = "gmbwe110@gmail.com";
+  copyToClipboard(textToCopy);
+  alert('Copied email: gmbwe110@gmail.com');
+});
 
